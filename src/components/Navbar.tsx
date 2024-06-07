55862144c91b2f63a82ab21174943769c92223cc
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-07 14:13:25 -0700

    add styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,16 +7,16 @@ type Props = {}
 
 export default function Navbar({}: Props) {
   return (
-    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
+    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-black'>
       <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
         <p className='flex items-center justify-center gap-2'>    
-          <h2 className='text-white text-3xl font-extrabold hover:text-blue-300 transition duration-300'>Weather</h2>
+          <h2 className='text-white text-3xl font-extrabold hover:text-yellow-300 transition duration-300'>Weather</h2>
           <FaSun className='text-3xl mt-1 text-yellow-300 animate-spin-slow'/>
         </p>
         {/*  */}
         <section className='flex gap-4 items-center'>
-          <FaLocationCrosshairs className='text-2xl text-white hover:text-blue-300 cursor-pointer transition duration-300'/>
-          <CiLocationOn className='text-3xl text-white hover:text-blue-300 cursor-pointer transition duration-300'/>
+          <FaLocationCrosshairs className='text-2xl text-white hover:text-yellow-300 cursor-pointer transition duration-300'/>
+          <CiLocationOn className='text-3xl text-white hover:text-yellow-300 cursor-pointer transition duration-300'/>
           <p className='text-white text-lg font-semibold'>Canada</p>
           <div>
             {/* search box */}
