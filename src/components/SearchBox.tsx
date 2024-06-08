d90bf95faa7556d40d2853d1c79bcc45387055d5
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-08 10:43:28 -0700

    add searchbox

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -6,9 +6,9 @@ type Props = {};
 export default function SearchBox({}: Props) {
   return (
     <form className="flex relative items-center justify-center h-10">
-      <input type="text" placeholder="Seach location..."/>
+      <input type="text" placeholder="Seach location..." className="px-4 py-4 w-[230px] border-gray-300 rounded-l-md focus:outline-none h-full"/>
       <button>
-        <IoIosSearch />
+        <IoIosSearch className="text-3xl text-white hover:text-yellow-300 cursor-pointer transition duration-300" />
       </button>
     </form>
   );
