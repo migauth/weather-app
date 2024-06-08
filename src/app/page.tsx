137bc01d73c86c0dcb9aadcff155e9f365a6d44f
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-08 10:42:48 -0700

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-   <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+   <div className="flex flex-col gap-4 bg-gray-300 min-h-screen">
     <Navbar />
    </div>
   );
