04e53da1ef5d4f763f0d78db6c0c8b2553d479b8
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-10 10:44:59 -0700

    add weather api, WeatherData type, tailwind-merge clsx, react-query, axios to page and use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,83 @@
+'use client'
+
 import Navbar from "@/components/Navbar";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+type WeatherData = {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: Array<{
+    dt: number;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+      temp_kf: number;
+    };
+    weather: Array<{
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }>;
+    clouds: {
+      all: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+    };
+    visibility: number;
+    pop: number;
+    rain?: {
+      '3h': number;
+    };
+    sys: {
+      pod: string;
+    };
+    dt_txt: string;
+  }>;
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+};
+
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>('repoData', async () =>
+    {
+      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`);
+      return data;
+    }
+  )
+
+  console.log(data?.city.name);
+
+  if (isLoading) 
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    )
+
   return (
    <div className="flex flex-col gap-4 bg-gray-300 min-h-screen">
     <Navbar />
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,14 +1,29 @@
+import cn from "@/utils/cn";
 import React from "react";
 import { IoIosSearch } from "react-icons/io";
 
-type Props = {};
+type Props = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+};
 
-export default function SearchBox({}: Props) {
+export default function SearchBox(props: Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
-      <input type="text" placeholder="Seach location..." className="px-4 py-4 w-[230px] border-gray-300 rounded-l-md focus:outline-none h-full"/>
-      <button>
-        <IoIosSearch className="text-3xl text-white hover:text-yellow-300 cursor-pointer transition duration-300" />
+    <form
+      onSubmit={props.onSubmit}
+      className={cn("flex relative items-center justify-center h-10", props.className)}
+    >
+      <input
+        type="text"
+        value={props.value}
+        onChange={props.onChange}
+        placeholder="Seach location..."
+        className="px-4 py-4 w-[230px] border-gray-300 rounded-l-md focus:outline-none h-full"
+      />
+      <button className="px-4 py-[5px] bg-yellow-400 text-3xl text-white hover:text-yellow-300 cursor-pointer transition duration-300 rounded-r-md ">
+        <IoIosSearch />
       </button>
     </form>
   );
