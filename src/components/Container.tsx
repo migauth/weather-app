febb843a004f4f23489222146fc13e0f736ad80f
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-10 14:46:52 -0700

    add Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function Container({}: Props) {
+  return (
+    <div className="w-full bg-white border rounded-x1 flex py-4 shadow-sm">
+      Container
+    </div>
+  );
+}
