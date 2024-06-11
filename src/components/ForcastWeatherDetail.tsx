d3d18b66c7ce0e8fedaf63743f82362816ca27ff
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-11 14:39:51 -0700

    add forcast weather detail component

---
+++ src/components/ForcastWeatherDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './Container'
+
+type Props = {}
+
+export default function ForcastWeatherDetail({}: Props) {
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  )
+}
