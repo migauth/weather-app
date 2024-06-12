6ee6916ea90c9d0c9ad63f92e9788e2e83fccd72
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-12 13:36:03 -0700

    add weather details component

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -63,7 +63,7 @@ function SingleWeatherDetail(props: SingleWeatherDetailProps) {
   return (
     <div className="flex flex-col justify-between gap-2 items-center text-xs font-semibold text-black/80">
       <p className="whitespace-nowrap">{props.information}</p>
-      <div className="text 3xl">{props.icon}</div>
+      <div className="text-3xl">{props.icon}</div>
       <p>{props.value}</p>
     </div>
   );
