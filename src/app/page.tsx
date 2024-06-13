1745e8047c028fe6a1657707d5fc7f039d1d8eb7
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-13 13:42:48 -0700

    add atom and add place value to api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,9 @@ import axios from "axios";
 import WeatherDetails from "@/components/WeatherDetails";
 import metersToKilometers from "@/utils/metersToKilometers";
 import convertWindSpeed from "@/utils/convertWindSpeed";
+import ForcastWeatherDetail from "@/components/ForcastWeatherDetail";
+import { useAtom } from "jotai";
+import { placeAtom } from "./atom";
 
 type WeatherData = {
   cod: string;
@@ -76,11 +79,13 @@ const formattedTime = `${hours}:${minutes}`;
 console.log(`The local time is: ${formattedTime}`);
 
 export default function Home() {
+  const [place, setPlace] = useAtom(placeAtom)
+
   const { isLoading, error, data } = useQuery<WeatherData>(
     "repoData",
     async () => {
       const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=sooke&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
       );
       return data;
     }
@@ -89,6 +94,23 @@ export default function Home() {
   const firstData = data?.list[0];
 
   console.log("data: ", data);
+
+  const uniqueDates = [
+    ...new Set(
+      data?.list.map(
+        (entry) => new Date(entry.dt * 1000).toISOString().split("T")[0]
+      )
+    ),
+  ];
+
+  // Filtering data to get the first entry after 6 AM for each unique date
+  const firstDataForEachDate = uniqueDates.map((date) => {
+    return data?.list.find((entry) => {
+      const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0];
+      const entryTime = new Date(entry.dt * 1000).getHours();
+      return entryDate === date && entryTime >= 6;
+    });
+  });
 
   if (isLoading)
     return (
@@ -159,28 +181,33 @@ export default function Home() {
             <div className=" flex gap-4">
               {/* left */}
               <Container className="w-fit justify-center flex-col px-4 items-center">
-                <p className=" capitalize text-center">{firstData?.weather[0].description}</p>
+                <p className=" capitalize text-center">
+                  {firstData?.weather[0].description}
+                </p>
                 <WeatherIcon
-                      iconname={getDayOrNightIcon(
-                        firstData?.weather[0].icon ?? '',
-                        firstData?.dt_txt ?? ""
-                      )}
-                      />
+                  iconname={getDayOrNightIcon(
+                    firstData?.weather[0].icon ?? "",
+                    firstData?.dt_txt ?? ""
+                  )}
+                />
               </Container>
               <Container className="bg-yellow-300 px-6 gap-4 justify-between overflow-x-auto">
-                <WeatherDetails 
-                visability={metersToKilometers(firstData?.visibility ?? 10000)} 
-                airPressure={`${firstData?.main.pressure} hPa`}
-                humidity={`${firstData?.main.humidity}%`}
-                windSpeed={convertWindSpeed(firstData?.wind.speed ?? 100)}
-                sunrise={format(
-                  fromUnixTime(data?.city.sunrise ?? 
-                  1718194404), "H:mm")}
-                sunset={format(
-                  fromUnixTime(data?.city.sunset ?? 
-                  1718252195), "H:mm")}
-                >
-                </WeatherDetails>
+                <WeatherDetails
+                  visability={metersToKilometers(
+                    firstData?.visibility ?? 10000
+                  )}
+                  airPressure={`${firstData?.main.pressure} hPa`}
+                  humidity={`${firstData?.main.humidity}%`}
+                  windSpeed={convertWindSpeed(firstData?.wind.speed ?? 100)}
+                  sunrise={format(
+                    fromUnixTime(data?.city.sunrise ?? 1718194404),
+                    "H:mm"
+                  )}
+                  sunset={format(
+                    fromUnixTime(data?.city.sunset ?? 1718252195),
+                    "H:mm"
+                  )}
+                ></WeatherDetails>
               </Container>
               {/* right */}
             </div>
@@ -189,6 +216,31 @@ export default function Home() {
         {/* 7 day forcast */}
         <section className="flex w-full flex-col gap-4">
           <p className="text 2xl">Forcast (7 days) </p>
+          {firstDataForEachDate.map((d, i) => (
+            <ForcastWeatherDetail 
+              key={i}
+              description={d?.weather[0].description ?? ""}
+              weatherIcon={d?.weather[0].icon ?? "01d"}
+              date={d ? format(parseISO(d.dt_txt), "dd.MM") : ""}
+              day={d ? format(parseISO(d.dt_txt), "dd.MM") : "EEEE"}
+              feels_like={d?.main.feels_like ?? 0}
+              temp={d?.main.temp ?? 0}
+              temp_max={d?.main.temp_max ?? 0}
+              temp_min={d?.main.temp_min ?? 0}
+              airPressure={`${d?.main.pressure} hPa `}
+              humidity={`${d?.main.humidity}% `}
+              sunrise={format(
+                fromUnixTime(data?.city.sunrise ?? 1702517657),
+                "H:mm"
+              )}
+              sunset={format(
+                fromUnixTime(data?.city.sunset ?? 1702517657),
+                "H:mm"
+              )}
+              visability={`${metersToKilometers(d?.visibility ?? 10000)} `}
+              windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)} `}
+              />
+          ))}
         </section>
       </main>
     </div>
